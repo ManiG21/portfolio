fdf4798a16998e2c744406c5150f32bb65a27f0f
ManiG21 <manigrant023@gmail.com>
2022-08-13 13:45:31 -0400

    intial portfolio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import Gohan from './Gohan-ssj2.gif';
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [search, setSearch] = useState("");
+  const incrementCount = () => setCount(count + 1);
+  const decrementCount = () => setCount(count - 1);
+
+  const updateSearch = e => {
+    setSearch(e.target.value)
+  }
+  const searchClick = () => {
+    window.open(`https://www.google.com/search?tbm=isch&q=${search}`);
+    setSearch("")
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className='Navbar'></div>
+      <h2>Welcome to my Portfolio</h2>
+      <img src="/img/giphy.gif" alt='Spider-man'></img>
+      <img src={Gohan} alt='Gohan'></img>
+      <button onClick={searchClick}>Search</button>
+      <input onInput={updateSearch} placeholder="image Search" value={search}/>
+      <label> Image Search</label>
+
+      <div className='click-container'>
+        <h3>Click Count {count}</h3>
+        <button onClick={incrementCount}>+</button>
+        <button onClick={decrementCount}>-</button>
+      </div>
     </div>
   );
 }
