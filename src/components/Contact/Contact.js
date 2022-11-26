4de30c894aeaabc2a3552e56bedb42f578a32787
ManiG21 <manigrant023@gmail.com>
2022-11-26 18:28:43 -0500

    projects animations

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -12,7 +12,7 @@ function Contact() {
       <div className="contact container">
 {/* <FontAwesomeIcon className='Emojis' id='Excited' icon={faFaceSmileBeam}/> */}
 
-         <header> My Contact</header>
+         <header> My Contacts</header>
          <a href='https://github.com/ManiG21'> <GoMarkGithub className='logos' /></a>
        <a href='https://www.linkedin.com/in/armani-grant-1a7b75227/'><AiFillLinkedin className='logos' /></a>
 
