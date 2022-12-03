8cd5e65ad59faebf063899bca576150cacbd5388
ManiG21 <manigrant023@gmail.com>
2022-12-02 23:15:05 -0500

    projects animations 2

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -14,8 +14,8 @@ function Project() {
             <a className='project-button'  id='Game' href='https://manig21.github.io/Game-Project/Sonic/sonic.html' target='_blank' rel='noreferrer' >Game Project</a>
             <a className='project-button'id='Movies-More' href='https://imaginative-unicorn-f88fec.netlify.app/' target='_blank' rel='noreferrer' >Movies & More</a>
             <a className='project-button' id='Pro' href='https://www.google.com/' target='_blank' rel='noreferrer' >Coming Soon</a>
-            {/* <a className='project-button' href=''> <div >Coming Soon</div></a>
-            <a className='project-button' href=''> <div >Coming Soon</div></a>  */}
+            {/* /* <a className='project-button' href=''> <div >Coming Soon</div></a> */}
+            
          </div>
       </div>
    )
