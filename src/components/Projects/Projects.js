7790b2153e41d4b2798a186bab3dc6e9bcec11f8
ManiG21 <manigrant023@gmail.com>
2022-12-07 22:46:29 -0500

    emoji comments

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -14,6 +14,12 @@ function Project() {
             <a className='project-button'id='Movies-More' href='https://imaginative-unicorn-f88fec.netlify.app/' target='_blank' rel='noreferrer' >Movies & More</a>
             <a className='project-button' id='Pro' href='https://www.google.com/' target='_blank' rel='noreferrer' >Coming Soon</a>
             {/* /* <a className='project-button' href=''> <div >Coming Soon</div></a> */}
+                 {/* <FontAwesomeIcon className='Emojis Excited' icon={faFaceSmileBeam} />
+                <FontAwesomeIcon className='Emojis Happy' icon={faFaceSmile} />
+                <FontAwesomeIcon className='Emojis Meh' icon={faFaceMeh} />
+                <FontAwesomeIcon className='Emojis Angry' icon={faFaceAngry} />
+                <FontAwesomeIcon className='Emojis Sad' icon={faFaceSadCry} />
+                <FontAwesomeIcon className='Emojis Sad' icon={faAngleDoubleLeft} /> */}
             
          </div>
       </div>
