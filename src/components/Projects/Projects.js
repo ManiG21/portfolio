cbe8c68f7bd359876e11d9443c9b99c7eae2b5a8
ManiG21 <manigrant023@gmail.com>
2023-01-11 14:47:32 -0500

    M & M & I

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -12,7 +12,7 @@ function Project() {
             <a className='project-button' id='Calc' href='https://manig21.github.io/Calculator-Project/' target='_blank' rel='noreferrer' >Calculator</a>
             <a className='project-button'  id='Game' href='https://manig21.github.io/Game-Project/Sonic/sonic.html' target='_blank' rel='noreferrer' >Game Project</a>
             <a className='project-button'id='Movies-More' href='https://imaginative-unicorn-f88fec.netlify.app/' target='_blank' rel='noreferrer' >Movies & More</a>
-            <a className='project-button' id='Pro' href='https://www.google.com/' target='_blank' rel='noreferrer' >Coming Soon</a>
+            <a className='project-button' id='Pro' href='https://main.d3156uqzn4jd72.amplifyapp.com/ ' target='_blank' rel='noreferrer' >Me Myself & I</a>
             {/* /* <a className='project-button' href=''> <div >Coming Soon</div></a> */}
                  {/* <FontAwesomeIcon className='Emojis Excited' icon={faFaceSmileBeam} />
                 <FontAwesomeIcon className='Emojis Sad' icon={faAngleDoubleLeft} /> */}
