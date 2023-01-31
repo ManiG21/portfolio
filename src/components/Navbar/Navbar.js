8e615910cddb7ee4264541b9899d01df61eb7683
ManiG21 <manigrant023@gmail.com>
2023-01-31 12:05:17 -0500

    changes Mobile view 1.0

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,13 +1,18 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import './Navbar.css';
 
 export default function Navbar() {
+    const [openMenu, setOpenMenu] = useState(false)
+    const toggleMenu = () => setOpenMenu(!openMenu)
     return <div className="nav-button">
-    <Link to="/"> <button className="buttonNav">Welcome</button></Link>
-    <Link to="/about"> <button className="buttonNav">About</button></Link>
-    <Link to="/projects"> <button className="buttonNav">Projects</button></Link>
-    <Link to="/contact"> <button className="buttonNav">Contact</button></Link>
-    {/* <a href="/Armani_Grant_Resume.docx"> My Resume</a> */}
-    
+        <button className="hamburger-menu buttonNav" onClick={toggleMenu}>☰</button>
+        <div className={`${openMenu ? "opened" : "closed"} navLinkContainer`}>
+            <Link to="/"> <button className="buttonNav">Welcome</button></Link>
+            <Link to="/about"> <button className="buttonNav">About</button></Link>
+            <Link to="/projects"> <button className="buttonNav">Projects</button></Link>
+            <Link to="/contact"> <button className="buttonNav">Contact</button></Link>
+            {/* <a href="/Armani_Grant_Resume.docx"> My Resume</a> */}
+        </div>
     </div>
 }
