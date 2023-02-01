f31c211f9c6f3b38544bf5de320598346a7b150e
ManiG21 <manigrant023@gmail.com>
2023-02-01 18:12:29 -0500

    Resume 0.1

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -10,7 +10,7 @@ function About() {
         <header>About Me</header>  
          <img className='logo'id='Me' src={Me} alt='Me'></img>
          <p className='para'>Hello my name is Armani Grant. I am a avid learner that is always looking for ways to improve to 
-            become the best I can be at my craft. I have a passion front-end website development. 
+            become the best I can be at my craft. I have a passion for front-end website development. 
             When designing I use my creativity and attention to detail to implement my ideas into code. 
             I aspire to go into to career in front-end website development and eventually UI/UX design as my pursuit for knowledge continues.
             {/* Here's a link to my resume <Link src= {Resume}>Resume</Link> */}
