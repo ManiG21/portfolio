1ee66ac9e1b462d0ebb1b72e58f3f9260b0417e8
ManiG21 <manigrant023@gmail.com>
2023-07-24 21:48:35 -0400

    rem link cleanup

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -8,16 +8,13 @@ import { AiFillLinkedin } from "react-icons/ai";
 function Contact() {
    return (
 
-      
       <div className="contact container">
 
          <header> My Contacts</header>
          <a href='https://github.com/ManiG21'> <GoMarkGithub className='logos logo' /></a>
          <a href='https://www.linkedin.com/in/armani-grant-1a7b75227/'><AiFillLinkedin id='w' className='logos logo' /></a>
-      {/*<a href='https://www.linkedin.com/in/armani-grant-1a7b75227/'><AiFillLinkedin className='logos logo' /></a> */}
-         
+      {/*<a href='https://www.linkedin.com/in/armani-grant-1a7b75227/'><AiFillLinkedin className='logos logo' /></a> */}  
       </div>
    )
-
 }
 export default Contact;
