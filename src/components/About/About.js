a3ed096ce0a7accf872db4869716715648ce081d
ManiG21 <manigrant023@gmail.com>
2023-07-26 18:16:11 -0400

    new about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,6 +1,7 @@
 import './About.css';
 import Me from './Me2.jpeg';
 import Chicken from './Chicken.JPG';
+import Gohan from './Gohan-ssj2.gif'
 // import Resume from './Armani_Grant_Resume.docx'
 
 function About() {
@@ -20,16 +21,17 @@ function About() {
            {/*Here's a link to my resume:  <Link src= {Resume}>Resume</Link> */} </div>
          </div>
 
-         <img className='logo'id='Chick' src={Chicken} alt='Me'></img>
-
-         <p className='para'> <h2 id='ab'>Personal Interests/Hobbies</h2>Here are a few things are enjoy doing 
-            in my free time. I really enjoy cooking, my favorite dish to cook is Chicken Parmagina. My favorite 
+         
+         <div className='para'> <h2 id='ab'>Personal Interests/Hobbies</h2> 
+        <div className='row'> <img className='logo'id='Chick' src={Chicken} alt='Chicken'></img>
+         <img className='logo'id='ssj' src={Gohan} alt='Gohan'></img>
+          <p>Here are a few things are enjoy doing  in my free time. I really enjoy cooking, my favorite dish to cook is Chicken Parmagina. My favorite 
             sport is basketball, I have played it for most of my life and still occasionally to this day. 
             I workout regularly whether it's lifting weights, calistentics or just simply going for a walk in 
             the park (at least once a day). Music is a constant in my life, it's something I listen to everyday and all different
             types depending the feel of the day. Some of my favorite artists are Michael Jackson, Ne-Yo, Alicia Keys
-            and Linkin Park.
-         </p>
+            and Linkin Park.</p> </div> 
+         </div>
     </div>
    ) 
     
