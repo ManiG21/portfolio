f1d0b40df4e07068ee0db6deb68f9d78afc59240
ManiG21 <manigrant023@gmail.com>
2023-07-26 17:53:35 -0400

    new container

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,14 +8,16 @@ function About() {
     <div className="about container">
        
         <header>About Me</header>  
-         <img className='logo'id='Me' src={Me} alt='Me'></img>
 
-         <p className='para'> <h2 id='ab'>Introduction </h2> Hello my name is Armani Grant. I am a avid 
+         <p className='para'> <h2 id='ab'>Introduction </h2>
+         <div className='row'> 
+         <img className='logo'id='Me' src={Me} alt='Me'></img>
+            Hello my name is Armani Grant. I am a avid 
             learner that is always looking for ways to improve to become the best I can be at my craft. 
             I have a passion for front-end website development. When designing I use my creativity and 
             attention to detail to implement my ideas into code. I aspire to go into to career in front-end 
             website development and eventually UI/UX design as my pursuit for knowledge continues.
-           {/*Here's a link to my resume:  <Link src= {Resume}>Resume</Link> */}
+           {/*Here's a link to my resume:  <Link src= {Resume}>Resume</Link> */} </div>
          </p>
 
          <img className='logo'id='Chick' src={Chicken} alt='Me'></img>
