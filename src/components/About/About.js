42b20b744e9c6ad38533a17d7464561a62a60f90
ManiG21 <manigrant023@gmail.com>
2023-07-26 22:56:18 -0400

    carousel notes

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,9 +2,15 @@ import './About.css';
 import Me from './Me2.jpeg';
 import Chicken from './Chicken.JPG';
 import Gohan from './Gohan-ssj2.gif'
+import { useState } from 'react';
 // import Resume from './Armani_Grant_Resume.docx'
 
 function About() {
+   // Create a variable to store where we are in the carousel
+   // Create a way to automatically increment the carousel number 
+      // When are our Carousel is out of images reset to the first one
+      // Need animations for images going out and in
+   // useState()
    return (
       <div className="about container">
 
@@ -28,13 +34,14 @@ function About() {
                <div>
                   <img className='logo' id='Chick' src={Chicken} alt='Chicken'></img>
                   <img className='logo' id='ssj' src={Gohan} alt='Gohan'></img>
-            </div>
+              </div>
                <p>Here are a few things are enjoy doing  in my free time. I really enjoy cooking, my favorite dish to cook is Chicken Parmagina. My favorite
                   sport is basketball, I have played it for most of my life and still occasionally to this day.
                   I workout regularly whether it's lifting weights, calistentics or just simply going for a walk in
                   the park (at least once a day). Music is a constant in my life, it's something I listen to everyday and all different
                   types depending the feel of the day. Some of my favorite artists are Michael Jackson, Ne-Yo, Alicia Keys
-                  and Linkin Park.</p> </div>
+                  and Linkin Park.</p> 
+            </div>
          </div>
       </div>
    )
