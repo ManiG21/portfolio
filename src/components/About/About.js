0159a3b66b4f61608088fa6a81ea6c5072ef2ecb
ManiG21 <manigrant023@gmail.com>
2023-07-30 23:49:56 -0400

    space

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -36,6 +36,7 @@ function About() {
                   <img className='logo' id='Chick' src={Chicken} alt='Chicken'></img>
                   <img className='logo' id='ssj' src={Gohan} alt='Gohan'></img>
               </div>
+              
                <p> Here are a few things are enjoy doing  in my free time. I really enjoy cooking, my favorite dish to cook is Chicken Parmagina. My favorite
                   sport is basketball, I have played it for most of my life and still occasionally to this day.
                   I workout regularly whether it's lifting weights, calistentics or just simply going for a walk in
