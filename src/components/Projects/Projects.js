2c32ced5e6c74914170b61f8b47832c5e8d8f081
ManiG21 <manigrant023@gmail.com>
2023-08-15 23:18:33 -0400

    clean up

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -14,7 +14,6 @@ function Project() {
             <a className='project-button'id='Movies-More' href='https://imaginative-unicorn-f88fec.netlify.app/' target='_blank' rel='noreferrer' >Movies & More</a>
             <a className='project-button' id='Pro' href='https://main.d3156uqzn4jd72.amplifyapp.com/ ' target='_blank' rel='noreferrer' >Me Myself & I</a>
                  {/* <FontAwesomeIcon className='Emojis Excited' icon={faFaceSmileBeam} />
-                <FontAwesomeIcon className='Emojis Sad' icon={faAngleDoubleLeft} /> */}
             
          </div>
       </div>
