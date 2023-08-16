e892fd5d79745a94b82fb2fa77b2b56f500ae44a
ManiG21 <manigrant023@gmail.com>
2023-08-15 23:19:12 -0400

    clean up

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -13,7 +13,6 @@ function Project() {
             <a className='project-button'  id='Game' href='https://manig21.github.io/Game-Project/Sonic/sonic.html' target='_blank' rel='noreferrer' >Game Project</a>
             <a className='project-button'id='Movies-More' href='https://imaginative-unicorn-f88fec.netlify.app/' target='_blank' rel='noreferrer' >Movies & More</a>
             <a className='project-button' id='Pro' href='https://main.d3156uqzn4jd72.amplifyapp.com/ ' target='_blank' rel='noreferrer' >Me Myself & I</a>
-            
          </div>
       </div>
    )
