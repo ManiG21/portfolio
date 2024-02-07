21233be8dfe3753273a327c9af1d7b7af31992df
ManiG21 <manigrant023@gmail.com>
2024-02-06 23:47:24 -0500

    new pro

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -13,6 +13,7 @@ function Project() {
              <source src={mult} type='video/mp4'></source>
            </video> */}
             <a className='project-button' id='Calc' href='https://manig21.github.io/Calculator-Project/' target='_blank' rel='noreferrer' >Calculator</a>
+            <a className='project-button' id='Calc' href='https://manig21.github.io/Calculator-Project/' target='_blank' rel='noreferrer' >Calculator</a>
             <a className='project-button'  id='Game' href='https://manig21.github.io/Game-Project/Sonic/sonic.html' target='_blank' rel='noreferrer' >Game Project</a>
             <a className='project-button'id='Movies-More' href='https://imaginative-unicorn-f88fec.netlify.app/' target='_blank' rel='noreferrer' >Movies & More</a>
             <a className='project-button' id='Pro' href='https://main.d3156uqzn4jd72.amplifyapp.com/ ' target='_blank' rel='noreferrer' >Me Myself & I</a>
